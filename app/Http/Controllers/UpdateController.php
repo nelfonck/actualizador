895955bc23005c12a5482b00c0bfd86239878985
nelfonck <nelfonck@gmail.com>
2023-10-29 10:31:28 -0600

    Update Controller added

---
+++ app/Http/Controllers/UpdateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class UpdateController extends Controller
+{
+    // Update the app
+    public function updateApp(Request $request){
+
+        Artisan::call('app:refresh', [
+            'user' => 1, '--queue' => 'default'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('updateapp', 'App\Http\Controllers\UpdateController@updateApp');
